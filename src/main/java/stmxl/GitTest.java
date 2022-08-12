97c14a4c4e7e26c74ef8d7ce644e9f8757fcca68
lgf <luogaofeng@stm-xl.com>
2022-08-12 15:50:11 +0800

    master commit3

--- src/main/java/stmxl/GitTest.java
+++ src/main/java/stmxl/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello git4");
         System.out.println("master commit2");
         System.out.println("hot-fix commit");
+        System.out.println("master commit3");
     }
 }
